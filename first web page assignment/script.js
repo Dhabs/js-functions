16f1dc080b6757ee2f3f7786ce039d0380e7e6d9
Uzoh Daberechi <omobadhabs@gmail.com>
2020-03-24 17:50:16 +0100

    java script assignment

---
+++ first web page assignment/script.js
@@ -0,0 +1,11 @@
+let name = "Daberechi";
+console.log("My name is:" + name)
+let courses = ["Frontend", "Design"]
+for (let i=0; i<courses.length; i++){
+    console.log(courses[i]);
+}
+for (let i=1; i<=200; i++) {
+    if (i % 2 ===0){
+        console.log(i);
+    }
+}
